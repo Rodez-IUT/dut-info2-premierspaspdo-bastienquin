69fce505c76f6d36ea01bc3cb3d5aba6452d3ce2
Bastien <bastien.quin@iut-rodez.fr>
2019-11-20 11:43:19 +0100

    fix #3.3 Activité 3 terminée

--- all_users.php
+++ all_users.php
@@ -62,12 +62,12 @@
 							$status_id = $_GET['status'];
 							$lettreDebut = $_GET['letter'];
 					
-							$stmt = $pdo->query('SELECT users.id AS id, username, email, name
+							$stmt = $pdo->query("SELECT users.id AS id, username, email, name
 												 FROM users
 												 JOIN status
 												 ON users.status_id = status.id
-												 WHERE status_id = '.$status_id.'
-												 AND username LIKE \''.$lettreDebut.'%'.'\'');
+												 WHERE status_id = $status_id
+												 AND username LIKE '$lettreDebut%'");
 							while ($row = $stmt->fetch()) {
 								echo "<tr>";
 									echo "<td>".$row['id']."</td>";
