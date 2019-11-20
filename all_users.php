ee8cd71a4bb01ba2d9435e2d269eda01adfdc9bd
Bastien <bastien.quin@iut-rodez.fr>
2019-11-20 11:33:55 +0100

    fix #3.2 Moteur de recherche sur status et username

--- all_users.php
+++ all_users.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 
-	<link href="../Bootstrap/css/bootstrap.css" rel="stylesheet"/>
+	<link href="css/bootstrap.css" rel="stylesheet"/>
 	<meta charset="utf-8" />
 	<title>Activite 2</title>
 
@@ -33,31 +33,50 @@
 			<div class="col-xs-12">
 			
 				<h1>All Users</h1>
+				<div class="formulaire"style="width:300px;">
+					<form action="all_users.php" method="get">
+						<p>Start with letter </p>
+						<input type="text" name="letter" class="form-control" <?php if(isset($_GET['letter'])){echo "value='".$_GET['letter']."' ";}?>/><br />
+						<p>And status is </p>
+						<select class="form-control" name="status">
+							<option value="1" <?php if(isset($_GET['letter']) && $_GET['status']){echo "selected";}?>>Waiting for account validation</option>
+							<option value="2" <?php if(isset($_GET['letter']) && $_GET['status']){echo "selected";}?>>Active account</option>
+						</select><br />
+						<input type="submit" value="OK" class="form-control" style="width: 50px;" /><br />
+					</form>
+				</div>
+				
 				<table class="table table-bordered table-striped">
-					<tr>
-						<th>Id</th>
-						<th>Username</th>
-						<th>Email</th>
-						<th>Status</th>
-					<tr>
 					
 					<?php
-						$status_id = 2;
-						$lettreDebut = 'e';
 					
-						$stmt = $pdo->query('SELECT users.id AS id, username, email, name
-											 FROM users
-											 JOIN status
-											 ON users.status_id = status.id
-											 WHERE status_id = '.$status_id.'
-											 AND username LIKE \''.$lettreDebut.'%'.'\'');
-						while ($row = $stmt->fetch()) {
+						if (isset($_GET['letter'])) {
+							
 							echo "<tr>";
-								echo "<td>".$row['id']."</td>";
-								echo "<td>".$row['username']."</td>";
-								echo "<td>".$row['email']."</td>";
-								echo "<td>".$row['name']."</td>";
-							echo "</tr>";
+								echo "<th>Id</th>";
+								echo "<th>Username</th>";
+								echo "<th>Email</th>";
+								echo "<th>Status</th>";
+							echo "<tr>";
+					
+							$status_id = $_GET['status'];
+							$lettreDebut = $_GET['letter'];
+					
+							$stmt = $pdo->query('SELECT users.id AS id, username, email, name
+												 FROM users
+												 JOIN status
+												 ON users.status_id = status.id
+												 WHERE status_id = '.$status_id.'
+												 AND username LIKE \''.$lettreDebut.'%'.'\'');
+							while ($row = $stmt->fetch()) {
+								echo "<tr>";
+									echo "<td>".$row['id']."</td>";
+									echo "<td>".$row['username']."</td>";
+									echo "<td>".$row['email']."</td>";
+									echo "<td>".$row['name']."</td>";
+								echo "</tr>";
+							}
+							
 						}
 					?>
 				</table>			
